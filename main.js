9010087538c9cdc73812233d310bae78eb91146b
Preet131 <90319247+Preet131@users.noreply.github.com>
2022-03-23 20:41:52 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,51 @@
+
+function setup() {
+    canvas = createCanvas(280, 280);
+    canvas.center();
+    background("white");
+    canvas.mouseReleased(classifyCanvas);
+    synth = window.speechSynthesis;
+}
+
+function preload(){
+    classifier = ml5.imageClassifier('DoodleNet');
+
+}
+function draw() {
+    strokeWeight(7);
+    stroke(0);
+    if(mouseIsPressed){
+        line(pmouseX, pmouseY, mouseX, mouseY);
+
+    }
+}
+
+
+function classifyCanvas() {
+    classifier.classify(canvas, gotResult);
+
+}
+
+function gotResult(error, results) {
+    if(error){
+        console.error(error);
+    }
+    console.log(results);
+    document.getElementById("label").innerHTML = "Label: "+results[0].label;
+    document.getElementById("confidence").innerHTML = "Accuracy: "+Math.round(results[0].confidence*100)+ "%";
+    utterThis = new SpeechSynthesisUtterance(results[0].label);
+    synth.speak(utterThis);
+}
+
+function clearCanvas() {
+    background("white")
+}
+
+function loadModal() {
+    $(document).ready(function(){ $("#introModal").modal('show'); });
+}
+
+function gotIt() {
+    $(document).ready(function(){ $("#introModal").modal('hide'); });
+}
+
